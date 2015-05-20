70f80c4c6a332e67c78d5e3dde6624388aab229e
bonafide666 <marpe82@gmail.com>
2015-05-20 15:00:06 +0200

    End of day Commit.
    Added more spawners. Need to change flight of meteors to get better gameplay. also might need to scale down more or at least set down speed of meteors as the screendistance is an issue ATM

--- Assets/MeteorStorm Assets/Prefabs/SpawnScript.cs
+++ Assets/MeteorStorm Assets/Prefabs/SpawnScript.cs
@@ -7,19 +7,40 @@ public class SpawnScript : MonoBehaviour {
 	public GameObject[] obj;
 	public int spawnMin;
 	public int spawnMax;
+	public bool GameRunning = false;
+	public bool SpawnInit = true;
 
 	// Use this for initialization
 	void Start () {
-		Spawn ();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		Debug.Log ("In Update");
+			if (Input.GetKeyDown(KeyCode.Space)) {
+			Debug.Log("Space Pressed");
+			GameRunning = !GameRunning;
+			SpawnInit = true;
+			}
+		if (GameRunning && SpawnInit) {
+			Spawn ();	
+				}
+
+		}
+
+	public void StartStop(){
+		GameRunning = !GameRunning;
+		SpawnInit = true;
+	}
+	void Spawn(){
+			if (GameRunning) {
+			Instantiate(obj[Random.Range(0,obj.Length)], transform.position, Quaternion.identity);
+			Invoke ("Spawn", Random.Range (spawnMin, spawnMax));
+			SpawnInit = false;
+		}
+			
+				}
+
 	}
 
-	void Spawn(){
-		Instantiate(obj[Random.Range(0,obj.Length)], transform.position, Quaternion.identity);
-		Invoke ("Spawn", Random.Range (spawnMin, spawnMax));
-	}
-}
