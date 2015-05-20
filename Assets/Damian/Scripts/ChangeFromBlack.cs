ed82e96e26ae1e6f53125027f3c8e623e0f58fa0
Damian Jørgensen <damek_bornholm@msn.com>
2015-05-20 10:47:39 +0200

    End of day commit.

---
+++ Assets/Damian/Scripts/ChangeFromBlack.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeFromBlack : MonoBehaviour
+{
+
+    public bool guess;
+    public bool notSwitched;
+
+    // Use this for initialization
+    void Start()
+    {
+        guess = false;
+        notSwitched = true;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (guess && notSwitched)
+        {
+            GameObject MyObjName = this.gameObject;
+            foreach (Transform child in MyObjName.transform)
+            {
+                if (child.gameObject.activeSelf)
+                    child.gameObject.SetActive(false);
+                else
+                    child.gameObject.SetActive(true);
+            }
+            notSwitched = false;
+        }
+
+        //if (guess)
+        //    gameObject.SetActive(false);
+        //else
+        //    gameObject.SetActive(true);
+
+    }
+}
---
+++ Assets/Damian/Scripts/flame.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class flame : MonoBehaviour
+{
+    int counter;
+
+    // Use this for initialization
+    void Start()
+    {
+        counter = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (counter < 5)
+        {
+            this.light.intensity = (float)1;
+        }
+        else if (counter > 5)
+        {
+            this.light.intensity = (float)1.1;
+
+            if (counter >= 10)
+            {
+                counter = 0;
+            }
+        }
+        counter++;
+
+    }
+}
